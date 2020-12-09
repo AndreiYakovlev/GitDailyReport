8122a0a631ae99583e1a4045bfab44836f85b426
AndreiYakovlev <gta.97.97@mail.ru>
2020-12-09 09:14:10 +0500

    Refact. Add pagination by left/right arrow

---
+++ Git Report Daily/CommitItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitDailyReport
+{
+   public class CommitItem
+   {
+      public string Message { get; set; }
+      public DateTimeOffset Date { get; set; }
+
+      public string Author { get; set; }
+   }
+}
--- Git Report Daily/Program.cs
+++ Git Report Daily/Program.cs
@@ -6,112 +6,121 @@ using System.Linq;
 using System.Reflection;
 using System.Threading;
 
-namespace Git_Report_Daily
+namespace GitDailyReport
 {
-	internal class CommitItem
-	{
-		public string Message { get; set; }
-		public DateTimeOffset Date { get; set; }
+   internal class Program
+   {
+      private static DateTimeOffset since = new DateTimeOffset(DateTime.Now.Date);
 
-		public string Author { get; set; }
-	}
+      private static void Main(string[] args)
+      {
+         try
+         {
+            var welcomeText = ReadWelcomFile("Welcome.txt");
+            PrintWelcome(welcomeText);
+         }
+         catch (Exception ex)
+         {
+            System.Diagnostics.Debug.WriteLine(ex.Message);
+         }
 
-	internal class Program
-	{
-		private static void Main(string[] args)
-		{
-			try
-			{
-				var welcomeText = ReadWelcomFile("Welcome.txt");
-				PrintWelcome(welcomeText);
-			}
-			catch (Exception ex)
-			{
-				System.Diagnostics.Debug.WriteLine(ex.Message);
-			}
+         try
+         {
+            string path = args.FirstOrDefault() ?? throw new Exception("Missing git path argument. Please add first $REPO arg to SourceTree action parameters");
+            Console.WriteLine($"Open git {path}");
+            if (Repository.IsValid(path))
+            {
+               using (var repo = new Repository(path))
+               {
+                  do
+                  {
+                     var commits = GetCommitsForDate(repo, since);
+                     Console.WriteLine($"Git daily report commits ({commits.Count}) for {since.Date.ToShortDateString()}");
+                     foreach (var item in commits.GroupBy(x => x.Author))
+                     {
+                        Console.WriteLine($"[Author {item.Key}]");
+                        item.ToList().ForEach(x => Console.WriteLine(x.Message.Trim()));
+                     }
+                     Console.WriteLine("-----------------------------------------------------------------------------");
+                  } while (ProcessInput());
+               }
+            }
+            else
+            {
+               throw new Exception($"Invalid git path {path}. Please add first $REPO arg");
+            }
+         }
+         catch (Exception ex)
+         {
+            Console.WriteLine(ex.Message);
+         }
+      }
 
-			try
-			{
-				string path = args.FirstOrDefault() ?? throw new Exception("Missing git path argument. Please add first $REPO arg to SourceTree action parameters");
-				Console.WriteLine($"Open git {path}");
-				if (Repository.IsValid(path))
-				{
-					using (var repo = new Repository(path))
-					{
-						var since = new DateTimeOffset(DateTime.Now.Date);
-						var commits = GetCommitsForDate(repo, since);
-						Console.WriteLine($"Git daily report commits ({commits.Count}) for {since.Date.ToShortDateString()}");
-						Console.WriteLine("-----------------------------------------------------------------------------");
-						foreach (var item in commits.GroupBy(x => x.Author))
-						{
-							Console.WriteLine($"[Author {item.Key}]");
-							item.ToList().ForEach(x => Console.WriteLine(x.Message.Trim()));
-							Console.WriteLine("-----------------------------------------------------------------------------");
-						}
-					}
-				}
-				else
-				{
-					throw new Exception($"Invalid git path {path}. Please add first $REPO arg");
-				}
-			}
-			catch (Exception ex)
-			{
-				Console.WriteLine(ex.Message);
-			}
+      private static List<CommitItem> GetCommitsForDate(Repository repository, DateTimeOffset since)
+      {
+         var filter = new CommitFilter();
+         var commitLog = repository.Commits.QueryBy(filter);
+         var commits = commitLog
+            .Where(c => c.Committer.When.Date == since)
+            .Select(x => new CommitItem
+            {
+               Message = x.Message,
+               Date = x.Committer.When,
+               Author = x.Committer.Name
+            }).ToList();
 
-			Console.ReadKey();
-		}
+         if (repository.Submodules.Any())
+         {
+            Console.WriteLine($"Lookup submodules ({repository.Submodules.Count()})...");
+         }
 
-		private static List<CommitItem> GetCommitsForDate(Repository repository, DateTimeOffset since)
-		{
-			var filter = new CommitFilter();
-			var commitLog = repository.Commits.QueryBy(filter);
-			var commits = commitLog
-				.Where(c => c.Committer.When > since)
-				.Select(x => new CommitItem
-				{
-					Message = x.Message,
-					Date = x.Committer.When,
-					Author = x.Committer.Name
-				}).ToList();
+         foreach (var sub in repository.Submodules)
+         {
+            using (var subRep = new Repository(sub.Path))
+            {
+               Console.WriteLine($"Submodule {sub.Name} opened");
+               commits.AddRange(GetCommitsForDate(subRep, since));
+            }
+         }
+         return commits;
+      }
 
-			if (repository.Submodules.Any())
-			{
-				Console.WriteLine($"Lookup submodules ({repository.Submodules.Count()})...");
-			}
+      private static string ReadWelcomFile(string filename)
+      {
+         var assembly = Assembly.GetExecutingAssembly();
+         string resourceName = assembly.GetManifestResourceNames().FirstOrDefault(str => str.EndsWith(filename));
 
-			foreach (var sub in repository.Submodules)
-			{
-				using (var subRep = new Repository(sub.Path))
-				{
-					Console.WriteLine($"Submodule {sub.Name} opened");
-					commits.AddRange(GetCommitsForDate(subRep, since));
-				}
-			}
-			return commits;
-		}
+         using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+         using (StreamReader reader = new StreamReader(stream))
+         {
+            return reader.ReadToEnd();
+         }
+      }
 
-		private static string ReadWelcomFile(string filename)
-		{
-			var assembly = Assembly.GetExecutingAssembly();
-			string resourceName = assembly.GetManifestResourceNames().FirstOrDefault(str => str.EndsWith(filename));
+      private static void PrintWelcome(string text)
+      {
+         var lines = text.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+         foreach (var line in lines)
+         {
+            Console.WriteLine(line);
+            Thread.Sleep(50);
+         }
+      }
 
-			using (Stream stream = assembly.GetManifestResourceStream(resourceName))
-			using (StreamReader reader = new StreamReader(stream))
-			{
-				return reader.ReadToEnd();
-			}
-		}
+      private static bool ProcessInput()
+      {
+         var key = Console.ReadKey();
+         switch (key.Key)
+         {
+            case ConsoleKey.LeftArrow:
+               since = since.AddDays(-1);
+               return true;
 
-		private static void PrintWelcome(string text)
-		{
-			var lines = text.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
-			foreach (var line in lines)
-			{
-				Console.WriteLine(line);
-				Thread.Sleep(50);
-			}
-		}
-	}
+            case ConsoleKey.RightArrow:
+               since = since.AddDays(1);
+               return true;
+         }
+         return false;
+      }
+   }
 }
